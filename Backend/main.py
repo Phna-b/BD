8c8a25e0f1badc0ad5a959ce65f792f12eee7de5
Pedro Henrique Nunes de Assis <phna8589@gmail.com>
2022-10-10 16:42:11 -0300

    Novo banco

---
+++ Backend/main.py
@@ -0,0 +1,15 @@
+import math
+
+def exercicio_4_2_2(n):
+    contador = 0
+    result = 1
+    cotrol = 1
+    while contador <= n:
+        if(n == 1): return n
+        else:
+        result += 1/control
+        contador += 1
+    return result
+
+
+print(exercicio_4_2_2(1))
